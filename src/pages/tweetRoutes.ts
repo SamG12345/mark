2795eb84bf2ee7c4a864bd5982fd5def055d0f73
SamG1234 <np03cs4s210297@heraldcollege.edu.np>
2023-08-25 00:04:29 +0545

    Tweet CRUD

--- src/pages/tweetRoutes.ts
+++ src/pages/tweetRoutes.ts
@@ -1,32 +1,60 @@
+import { Prisma, PrismaClient } from '@prisma/client';
 import { Router } from 'express';
 
 const router = Router();
+const prisma = new PrismaClient();
 
-router.post('/', (req, res) => {
-    res.status(501).json({ error : 'Not Implemented' });
+// Create Tweet
+router.post('/', async (req, res) => {
+    const { content, image, userId } = req.body;
+
+    try{
+        const result = await prisma.tweet.create({
+            data : {
+                content,
+                image,
+                userId, // TODO mange user based on the auth user
+            },
+        });
+        res.json(result);
+    }catch(e){
+        res.status(400).json({error : 'Username and Email must be Unique'})
+    }
     
 });
 
-router.get('/', (req, res) => {
-    res.status(501).json({ error : 'Not Implemented' });
+// list Tweets
+router.get('/', async(req, res) => {
+    const allTweets = await prisma.tweet.findMany();
+
+    res.json(allTweets);
     
 });
 
-router.get('/:id', (req, res) => {
+// Get one Tweet
+router.get('/:id', async(req, res) => {
     const {id} = req.params;
-    res.status(501).json({ error : 'Not Implemented' });
+    const tweet = await prisma.tweet.findUnique({ where : {id : Number(id) }})
+    if (!tweet){
+        return res.status(404).json({ error : 'Not Implemented' });
+    }
+    res.json(tweet);
+    
     
 });
 
+// Update Tweet
 router.put('/:id', (req, res) => {
     const {id} = req.params;
     res.status(501).json({ error : 'Not Implemented' });
     
 });
 
-router.delete('/:id', (req, res) => {
+// Delete Tweet
+router.delete('/:id', async(req, res) => {
     const {id} = req.params;
-    res.status(501).json({ error : 'Not Implemented' });
+    await prisma.tweet.delete({ where : { id : Number(id) }});
+    res.sendStatus(200);
     
 });
 
